e0c57f4eff811e0297749adc6f5c484caf26090d
Obinna <obinna.aguwa@gmail.com>
2022-11-11 13:30:24 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Obinna Aguwa on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
