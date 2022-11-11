668a6a4f6e8e84607c87afb72cacc9cb8a9d0087
Obinna <obinna.aguwa@gmail.com>
2022-11-11 14:57:19 +0100

    appetizer model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,23 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Obinna Aguwa on 11/11/2022.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse: Decodable {
+    let request: [Appetizer]
+}
