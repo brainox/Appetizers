c9a3ff192d99363aefee62aa9dbfd561877a0670
Obinna <obinna.aguwa@gmail.com>
2022-11-14 03:49:59 +0100

    appetiser remote image displayed

--- Appetizers/AppetizerListCell.swift
+++ Appetizers/AppetizerListCell.swift
@@ -11,15 +11,15 @@ struct AppetizerListCell: View {
     let appetizer: Appetizer
     var body: some View {
         HStack {
-            Image("carplay")
-                .resizable()
+            
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
-                    .font(.title)
+                    .font(.title2)
                     .fontWeight(.medium)
                 
                 Text("$\(appetizer.price, specifier: "%.2f")")
